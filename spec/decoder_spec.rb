d3f4af5321b036eae6638d563dcec739cf29a827
Jesus Rodriguez <Foxandxss@gmail.com>
2014-07-07 16:00:20 +0200

    Order tests into describe blocks

--- spec/decoder_spec.rb
+++ spec/decoder_spec.rb
@@ -1,146 +1,154 @@
 require 'spec_helper'
 
+# All jwt used here have been created by external tools to isolate the tests
+
 describe 'Decoder' do
   before do
     @payload = { 'hello' => 'world', 'iat' => 946681200 }
   end
 
-  it 'decodes a valid JWT using HS' do
-    secret          = 'mysecret'
-    jwt             = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
-    payload, header = JWT.decode(jwt, secret)
+  describe 'decoding' do
+    it 'decodes a valid JWT using HS' do
+      secret          = 'mysecret'
+      jwt             = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
+      payload, header = JWT.decode(jwt, secret)
 
-    expect(payload).to eq(@payload)
-  end
+      expect(payload).to eq(@payload)
+    end
 
-  it 'decodes a valid JWT using RS' do
-    public_key = OpenSSL::PKey::RSA.new(<<-KEY)
+    it 'decodes a valid JWT using RS' do
+      public_key = OpenSSL::PKey::RSA.new(<<-KEY)
 -----BEGIN PUBLIC KEY-----
 MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAUTQbhg/Hcgq26wY9yOWu9L6OX4fqekqo
 R+YhQUXaJGXhW0tNapTdIwTAycby0cUCXoA/7IYi4SXSKMT0owQxeQIDAQAB
 -----END PUBLIC KEY-----
-KEY
+      KEY
 
-    jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.A0Qiqmo02gEviZtQW8ESC94oKLXcgCSGnktyxDQZNE0AfSoMJZ9JMEWNjEXyp6k7VEm9nn9ZbEu2GdmAIJz-xw'
-    payload, header = JWT.decode(jwt, public_key, algorithm: 'RS256')
+      jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.A0Qiqmo02gEviZtQW8ESC94oKLXcgCSGnktyxDQZNE0AfSoMJZ9JMEWNjEXyp6k7VEm9nn9ZbEu2GdmAIJz-xw'
+      payload, header = JWT.decode(jwt, public_key, algorithm: 'RS256')
 
-    expect(payload).to eq(@payload)
+      expect(payload).to eq(@payload)
+    end
+
+    describe 'claims' do
+      it 'validates a JWT with the correct audience' do
+        expected_payload = { 'hello' => 'world', 'iat' => 946681200, 'aud' => 'rubyist' }
+        secret           = 'mysecret'
+        jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImF1ZCI6InJ1Ynlpc3QifQ.u6DB-dpK5zxZUmgprzXtg5yY4djsLLtIv4EAgE_nWSM'
+
+        payload, header = JWT.decode(jwt, secret, claims: { aud: 'rubyist' })
+
+        expect(payload).to eq(expected_payload)
+      end
+
+      it 'validates a JWT with expiry if it didnt expire yet' do
+        # The expiry is set to 24 hours
+        expected_payload = { 'hello' => 'world', 'iat' => 946681200, 'exp' => 946767600}
+        secret           = 'mysecret'
+        jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImV4cCI6OTQ2NzY3NjAwfQ.AZ9a_9L8oY95OT7jc019ZxjWc9fcT5chYsZy6QUgzj0'
+
+        # Travel 12 hours in the future
+        Timecop.travel(Time.at(946724400))
+
+        payload, header = JWT.decode(jwt, secret)
+
+        expect(payload).to eq(expected_payload)
+      end
+
+      it 'validates a JWT with the correct issuer' do
+        expected_payload = { 'hello' => 'world', 'iat' => 946681200, 'iss' => 'toni' }
+        secret           = 'mysecret'
+        jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImlzcyI6InRvbmkifQ.I4EoPQzt97rehCBnU8VTcDH5R2KX6b8Ta1t5bANGorc'
+
+        payload, header = JWT.decode(jwt, secret, claims: { iss: 'toni' })
+
+        expect(payload).to eq(expected_payload)
+      end
+    end
   end
 
-  it 'raises an error if you use a string as a key in RS decoding' do
-    secret = 'mysecret'
-    jwt    = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.E0f3M2R4p1_pEmDFp3PZNfXwLp6m3z7MNX468o8gQCYeslnOMdCGvzl1pCJhDs5M7KnaSlvPm_Be3WjYrk8ZDQ'
+  describe 'exceptions' do
+    it 'raises an error if you use a string as a key in RS decoding' do
+      secret = 'mysecret'
+      jwt    = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.E0f3M2R4p1_pEmDFp3PZNfXwLp6m3z7MNX468o8gQCYeslnOMdCGvzl1pCJhDs5M7KnaSlvPm_Be3WjYrk8ZDQ'
 
-    expect { JWT.decode(jwt, secret, algorithm: 'RS256') }
+      expect { JWT.decode(jwt, secret, algorithm: 'RS256') }
       .to raise_error JWT::Verificator::ClassMethods::InvalidKeyFormatError
-  end
+    end
 
-  it 'raises an error if you dont use a string as a key in HS decoding' do
-    key = OpenSSL::PKey::RSA.new(<<-KEY)
+    it 'raises an error if you dont use a string as a key in HS decoding' do
+      key = OpenSSL::PKey::RSA.new(<<-KEY)
 -----BEGIN PUBLIC KEY-----
 MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAUTQbhg/Hcgq26wY9yOWu9L6OX4fqekqo
 R+YhQUXaJGXhW0tNapTdIwTAycby0cUCXoA/7IYi4SXSKMT0owQxeQIDAQAB
 -----END PUBLIC KEY-----
-KEY
+      KEY
 
-    jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.E0f3M2R4p1_pEmDFp3PZNfXwLp6m3z7MNX468o8gQCYeslnOMdCGvzl1pCJhDs5M7KnaSlvPm_Be3WjYrk8ZDQ'
+      jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.E0f3M2R4p1_pEmDFp3PZNfXwLp6m3z7MNX468o8gQCYeslnOMdCGvzl1pCJhDs5M7KnaSlvPm_Be3WjYrk8ZDQ'
 
-    expect { JWT.decode(jwt, key) }
+      expect { JWT.decode(jwt, key) }
       .to raise_error JWT::Verificator::ClassMethods::InvalidKeyFormatError
-  end
+    end
 
-  it 'raises an error if the token doesnt have the correct number of segments' do
-    secret = 'mysecret'
-    jwt    = 'header.payload.signature.hello?'
+    it 'raises an error if the token doesnt have the correct number of segments' do
+      secret = 'mysecret'
+      jwt    = 'header.payload.signature.hello?'
 
-    expect { JWT.decode(jwt, key) }
+      expect { JWT.decode(jwt, key) }
       .to raise_error StandardError
-  end
+    end
 
-  it 'raises an error if the token cant be verified with the current secret key' do
-    secret = 'myothersecret'
-    jwt    = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.Gc_-AK7EN0nCQj6egXy525yk_cssK2A2lgX-w2NM90M'
+    it 'raises an error if the token cant be verified with the current secret key' do
+      secret = 'myothersecret'
+      jwt    = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIn0.Gc_-AK7EN0nCQj6egXy525yk_cssK2A2lgX-w2NM90M'
 
-    expect { JWT.decode(jwt, secret) }
+      expect { JWT.decode(jwt, secret) }
       .to raise_error StandardError
-  end
+    end
 
-  it 'raises an error if there is no header' do
-    secret = 'myothersecret'
-    jwt    = '.eyJoZWxsbyI6IndvcmxkIn0.Gc_-AK7EN0nCQj6egXy525yk_cssK2A2lgX-w2NM90M'
+    it 'raises an error if there is no header' do
+      secret = 'myothersecret'
+      jwt    = '.eyJoZWxsbyI6IndvcmxkIn0.Gc_-AK7EN0nCQj6egXy525yk_cssK2A2lgX-w2NM90M'
 
-    expect { JWT.decode(jwt, secret) }
-    .to raise_error StandardError
-  end
-
-  it 'raises an error if there is no payload' do
-    secret = 'myothersecret'
-    jwt    = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9..Gc_-AK7EN0nCQj6egXy525yk_cssK2A2lgX-w2NM90M'
-
-    expect { JWT.decode(jwt, secret) }
-    .to raise_error StandardError
-  end
-
-  it 'validates a JWT with the correct audience' do
-    expected_payload = { 'hello' => 'world', 'iat' => 946681200, 'aud' => 'rubyist' }
-    secret           = 'mysecret'
-    jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImF1ZCI6InJ1Ynlpc3QifQ.u6DB-dpK5zxZUmgprzXtg5yY4djsLLtIv4EAgE_nWSM'
-
-    payload, header = JWT.decode(jwt, secret, claims: { aud: 'rubyist' })
-
-    expect(payload).to eq(expected_payload)
-  end
-
-  it 'raises an error if the audience doesnt match' do
-    secret           = 'mysecret'
-    jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImF1ZCI6InJ1Ynlpc3QifQ.u6DB-dpK5zxZUmgprzXtg5yY4djsLLtIv4EAgE_nWSM'
-
-    expect { JWT.decode(jwt, secret, claims: { aud: 'phpers' })}
+      expect { JWT.decode(jwt, secret) }
       .to raise_error StandardError
-  end
+    end
 
-  it 'validates a JWT with the correct issuer' do
-    expected_payload = { 'hello' => 'world', 'iat' => 946681200, 'iss' => 'toni' }
-    secret           = 'mysecret'
-    jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImlzcyI6InRvbmkifQ.I4EoPQzt97rehCBnU8VTcDH5R2KX6b8Ta1t5bANGorc'
+    it 'raises an error if there is no payload' do
+      secret = 'myothersecret'
+      jwt    = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9..Gc_-AK7EN0nCQj6egXy525yk_cssK2A2lgX-w2NM90M'
 
-    payload, header = JWT.decode(jwt, secret, claims: { iss: 'toni' })
-
-    expect(payload).to eq(expected_payload)
-  end
-
-  it 'raises an error if the issuer doesnt match' do
-    secret           = 'mysecret'
-    jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImlzcyI6InRvbmkifQ.I4EoPQzt97rehCBnU8VTcDH5R2KX6b8Ta1t5bANGorc'
-
-    expect { JWT.decode(jwt, secret, claims: { iss: 'john' })}
-    .to raise_error StandardError
-  end
-
-  it 'validates a JWT with expiry if it didnt expire yet' do
-    # The expiry is set to 24 hours
-    expected_payload = { 'hello' => 'world', 'iat' => 946681200, 'exp' => 946767600}
-    secret           = 'mysecret'
-    jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImV4cCI6OTQ2NzY3NjAwfQ.AZ9a_9L8oY95OT7jc019ZxjWc9fcT5chYsZy6QUgzj0'
-
-    # Travel 12 hours in the future
-    Timecop.travel(Time.at(946724400))
-
-    payload, header = JWT.decode(jwt, secret)
-
-    expect(payload).to eq(expected_payload)
-  end
-
-  it 'raises an error if the token expired' do
-    # The expiry is set to 24 hours
-    secret           = 'mysecret'
-    jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImV4cCI6OTQ2NzY3NjAwfQ.AZ9a_9L8oY95OT7jc019ZxjWc9fcT5chYsZy6QUgzj0'
-
-    # Travel 24 hours, 1 second in the future
-    Timecop.travel(Time.at(946767601))
-
-    expect { JWT.decode(jwt, secret) }
+      expect { JWT.decode(jwt, secret) }
       .to raise_error StandardError
+    end
+
+    it 'raises an error if the audience doesnt match' do
+      secret           = 'mysecret'
+      jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImF1ZCI6InJ1Ynlpc3QifQ.u6DB-dpK5zxZUmgprzXtg5yY4djsLLtIv4EAgE_nWSM'
+
+      expect { JWT.decode(jwt, secret, claims: { aud: 'phpers' })}
+      .to raise_error StandardError
+    end
+
+    it 'raises an error if the issuer doesnt match' do
+      secret           = 'mysecret'
+      jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImlzcyI6InRvbmkifQ.I4EoPQzt97rehCBnU8VTcDH5R2KX6b8Ta1t5bANGorc'
+
+      expect { JWT.decode(jwt, secret, claims: { iss: 'john' })}
+      .to raise_error StandardError
+    end
+
+    it 'raises an error if the token expired' do
+      # The expiry is set to 24 hours
+      secret           = 'mysecret'
+      jwt              = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImV4cCI6OTQ2NzY3NjAwfQ.AZ9a_9L8oY95OT7jc019ZxjWc9fcT5chYsZy6QUgzj0'
+
+      # Travel 24 hours, 1 second in the future
+      Timecop.travel(Time.at(946767601))
+
+      expect { JWT.decode(jwt, secret) }
+      .to raise_error StandardError
+    end
   end
 end
 
--- spec/encoder_spec.rb
+++ spec/encoder_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
 
+# All expected_jwt used here have been created by external tools to isolate the tests
+
 describe 'Encoder' do
   before do
     Timecop.freeze(Time.at(946681200))
     @payload = { hello: 'world' }
   end
 
-  it 'encodes a JWT using HS' do
-    secret       = 'mysecret'
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
-    jwt          = JWT.encode(@payload, secret)
+  describe 'encoding' do
+    it 'encodes a JWT using HS' do
+      secret       = 'mysecret'
+      expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
+      jwt          = JWT.encode(@payload, secret)
 
-    expect(jwt).to eq(expected_jwt)
-  end
+      expect(jwt).to eq(expected_jwt)
+    end
 
-  it 'encodes a JWT using RS' do
-    key = OpenSSL::PKey::RSA.new(<<-KEY)
+    it 'encodes a JWT using RS' do
+      key = OpenSSL::PKey::RSA.new(<<-KEY)
 -----BEGIN RSA PRIVATE KEY-----
 MIIBOAIBAAJAUTQbhg/Hcgq26wY9yOWu9L6OX4fqekqoR+YhQUXaJGXhW0tNapTd
 IwTAycby0cUCXoA/7IYi4SXSKMT0owQxeQIDAQABAkBI9IfF6mdGDlpIzVK1K6YE
@@ -25,64 +28,69 @@ AiEAoGHNoRMeBnMC7mwbi8rf8RFriWpTp91IoFzD+oPV/fECIQCBnb3Tn4Uxccmd
 gp2rlfnMAgECIAfVedilho5Te0UQCZ4JRv0Z98zgT5JyLZf3+uu6L9/JAiBubUAY
 c1CuIOv3cWDJPNMWI57s1U2WF+zbtIkc1zr5+Q==
 -----END RSA PRIVATE KEY-----
-KEY
-    jwt          = JWT.encode(@payload, key, algorithm: 'RS256')
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.A0Qiqmo02gEviZtQW8ESC94oKLXcgCSGnktyxDQZNE0AfSoMJZ9JMEWNjEXyp6k7VEm9nn9ZbEu2GdmAIJz-xw'
-    expect(jwt).to eq(expected_jwt)
+      KEY
+      jwt          = JWT.encode(@payload, key, algorithm: 'RS256')
+      expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.A0Qiqmo02gEviZtQW8ESC94oKLXcgCSGnktyxDQZNE0AfSoMJZ9JMEWNjEXyp6k7VEm9nn9ZbEu2GdmAIJz-xw'
+      expect(jwt).to eq(expected_jwt)
+    end
+
+    it 'can use a payload with string as keys' do
+      payload      = { 'hello' => 'world' }
+      secret       = 'mysecret'
+      expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
+      jwt          = JWT.encode(payload, secret)
+
+      expect(jwt).to eq(expected_jwt)
+    end
+
+    it 'can use old ruby hash style' do
+      payload      = { :hello => 'world' }
+      secret       = 'mysecret'
+      expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
+      jwt          = JWT.encode(payload, secret)
+
+      expect(jwt).to eq(expected_jwt)
+    end
+
+    describe 'claims' do
+      it 'allows you to set an expiry time' do
+        secret       = 'mysecret'
+        expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImV4cCI6OTQ2Njg3MjAwfQ.zbalHdCubQpn_U32jW1dv9L2p3vWSgf-UsKwnEiFxWo'
+        jwt          = JWT.encode(@payload, secret, claims: { exp: 6000 })
+
+        expect(jwt).to eq(expected_jwt)
+      end
+
+      it 'allows you to set an audience' do
+        secret       = 'mysecret'
+        expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImF1ZCI6InJ1Ynlpc3QifQ.u6DB-dpK5zxZUmgprzXtg5yY4djsLLtIv4EAgE_nWSM'
+        jwt          = JWT.encode(@payload, secret, claims: { aud: 'rubyist' })
+
+        expect(jwt).to eq(expected_jwt)
+      end
+
+      it 'allows you to set an issuer' do
+        secret       = 'mysecret'
+        expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImlzcyI6InRvbmkifQ.I4EoPQzt97rehCBnU8VTcDH5R2KX6b8Ta1t5bANGorc'
+        jwt          = JWT.encode(@payload, secret, claims: { iss: 'toni' })
+
+        expect(jwt).to eq(expected_jwt)
+      end
+    end
   end
 
-  it 'raises NotImplementedError using a fake signature' do
-    secret = 'mysecret'
-    expect { JWT.encode(@payload, secret, algorithm: 'HS9888889') }
-      .to raise_error NotImplementedError
-  end
+  describe 'exceptions' do
+    it 'raises NotImplementedError using a fake signature' do
+      secret = 'mysecret'
+      expect { JWT.encode(@payload, secret, algorithm: 'HS9888889') }
+        .to raise_error NotImplementedError
+    end
 
-  it 'raises an error if you dont use a private key on RS algorithms' do
-    secret = 'mysecret'
-    expect { JWT.encode(@payload, secret, algorithm: 'RS256') }
-      .to raise_error StandardError
-  end
-
-  it 'can use a payload with string as keys' do
-    payload      = { 'hello' => 'world' }
-    secret       = 'mysecret'
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
-    jwt          = JWT.encode(payload, secret)
-
-    expect(jwt).to eq(expected_jwt)
-  end
-
-  it 'can use old ruby hash style' do
-    payload      = { :hello => 'world' }
-    secret       = 'mysecret'
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDB9.9OzXVFu0LK4miYiSkWrP5vhzHVtz5DHC8dWalkTah3c'
-    jwt          = JWT.encode(payload, secret)
-
-    expect(jwt).to eq(expected_jwt)
-  end
-
-  it 'allows you to set an expiry time' do
-    secret       = 'mysecret'
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImV4cCI6OTQ2Njg3MjAwfQ.zbalHdCubQpn_U32jW1dv9L2p3vWSgf-UsKwnEiFxWo'
-    jwt          = JWT.encode(@payload, secret, claims: { exp: 6000 })
-
-    expect(jwt).to eq(expected_jwt)
-  end
-
-  it 'allows you to set an audience' do
-    secret       = 'mysecret'
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImF1ZCI6InJ1Ynlpc3QifQ.u6DB-dpK5zxZUmgprzXtg5yY4djsLLtIv4EAgE_nWSM'
-    jwt          = JWT.encode(@payload, secret, claims: { aud: 'rubyist' })
-
-    expect(jwt).to eq(expected_jwt)
-  end
-
-  it 'allows you to set an issuer' do
-    secret       = 'mysecret'
-    expected_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0Ijo5NDY2ODEyMDAsImlzcyI6InRvbmkifQ.I4EoPQzt97rehCBnU8VTcDH5R2KX6b8Ta1t5bANGorc'
-    jwt          = JWT.encode(@payload, secret, claims: { iss: 'toni' })
-
-    expect(jwt).to eq(expected_jwt)
+    it 'raises an error if you dont use a private key on RS algorithms' do
+      secret = 'mysecret'
+      expect { JWT.encode(@payload, secret, algorithm: 'RS256') }
+        .to raise_error StandardError
+    end
   end
 end
 
